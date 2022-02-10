75d72fd31f288aff1ce3734919852df1be2b9177
Moisur <mosiurislamwebdesign@gmail.com>
2022-02-10 18:24:56 +0600

    complid

---
+++ js/bank.js
@@ -0,0 +1,37 @@
+// add ammount ;
+document.getElementById('deposit-button').addEventListener ("click", function(){
+    const depositinput = document.getElementById("deposit-input");
+    depositammount = depositinput.value;
+    const ammoutadd = document.getElementById("totaldeposit");
+    // console.log(ammoutadd)
+    const totalammotadd = ammoutadd.innerText;
+    // console.log(totalammotadd)
+    ammoutadd.innerText = parseFloat(totalammotadd)+ parseFloat(depositammount)
+    depositinput.value ='';
+    // update account balance 
+    const updatebalanceadd = document.getElementById("updatebalence");
+    const updatebalanceaddtext = updatebalanceadd.innerText;
+    const updatebalanceaddnumber = parseFloat(updatebalanceaddtext);
+    updatebalanceadd.innerText = updatebalanceaddnumber+parseFloat(totalammotadd)+ parseFloat(depositammount)
+ 
+});
+// withdraw ammout 
+document.getElementById('withdraw-button').addEventListener ("click", function(){
+   const withdrawinput = document.getElementById("withdraw-input");
+   const withdrawamount = withdrawinput.value;
+   const withdrawamountNumber = parseFloat(withdrawamount);
+// witgdeoammount  pat
+const withdrawamounttotal = document.getElementById("witgdeoammount");
+const withdrawamounttotalText = withdrawamounttotal.innerText;
+const withdrawamounttotalNumber = parseFloat(withdrawamounttotalText);
+withdrawamounttotal.innerText = withdrawamountNumber+withdrawamounttotalNumber;
+withdrawinput.value ='';
+   
+//    if(withdrawamount == "" ||typeof withdrawamount == "toString" ){
+//        alert("Any Number")
+//    }
+//    else{
+//        console.log(withdrawamount)
+//    }
+
+});
